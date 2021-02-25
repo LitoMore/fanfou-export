c1d1a01939960d16c67ccf14f794d6d6c3e55587
LitoMore <litomore@gmail.com>
2021-02-26 00:38:49 +0800

    Updates

---
+++ precache-manifest.89a85d4e91ccbfe5b2710e649b6ba6f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc693935ba060a50b8f2ecbbefdfba4e",
+    "url": "/fanfou-export/index.html"
+  },
+  {
+    "revision": "a74e39ba62f75aa22e9b",
+    "url": "/fanfou-export/static/css/2.4d48fd3d.chunk.css"
+  },
+  {
+    "revision": "44d398644dd427a9811d",
+    "url": "/fanfou-export/static/css/main.198c4dde.chunk.css"
+  },
+  {
+    "revision": "a74e39ba62f75aa22e9b",
+    "url": "/fanfou-export/static/js/2.ce6d036f.chunk.js"
+  },
+  {
+    "revision": "5ee5a9d777654e8bfb46e2f2323b46bc",
+    "url": "/fanfou-export/static/js/2.ce6d036f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d398644dd427a9811d",
+    "url": "/fanfou-export/static/js/main.4ad0e5e5.chunk.js"
+  },
+  {
+    "revision": "d9e0ff8641db6267c80d",
+    "url": "/fanfou-export/static/js/runtime-main.e68491d3.js"
+  },
+  {
+    "revision": "783bb457ed9b3294fe72f4b4bc9358a9",
+    "url": "/fanfou-export/static/media/Zpix.783bb457.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fanfou-export/precache-manifest.1c8de785d32208e7d39c29b4cb392e60.js"
+  "/fanfou-export/precache-manifest.89a85d4e91ccbfe5b2710e649b6ba6f6.js"
 );
 
 self.addEventListener('message', (event) => {
