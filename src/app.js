2d3ac675f58a891326d38c27769b9fe713600ebd
LitoMore <litomore@gmail.com>
2021-02-27 23:36:10 +0800

    Polish tips

--- src/app.js
+++ src/app.js
@@ -323,8 +323,8 @@ class App extends React.Component {
 									</p>
 									{done && <p>获取完毕。</p>}
 									{done && this.exportTypes()}
-									{fullList.length > 1000 && (
-										<p>
+									{done && fullList.length > 1000 && (
+										<p style={{color: 'grey'}}>
 											消息数量超过 1000 条，PDF 类型在线导出较慢。建议选择
 											MARKDOWN 导出后自行使用其他工具转换为 PDF。
 										</p>
